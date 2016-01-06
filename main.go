53fdf723624966f5b2e485094ea3f70ce4402c5f
Sven Bohm <bohms@msu.edu>
2016-01-06 11:21:32 -0500

    updated to allow the battery voltage field to be specified as a string

--- main.go
+++ main.go
@@ -26,6 +26,26 @@ func init() {
 	prometheus.MustRegister(batteryVoltage)
 }
 
+type stringSlice []string
+
+func (slice stringSlice) pos(value string) int {
+	for p, v := range slice {
+		if v == value {
+			return p
+		}
+	}
+	return -1
+}
+
+func readCSVLine(text string) []string {
+	reader := csv.NewReader(strings.NewReader(text))
+	fields, err := reader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fields
+}
+
 func loadData(fileName string) {
 
 	t, err := tail.TailFile(fileName, tail.Config{
@@ -36,24 +56,28 @@ func loadData(fileName string) {
 		log.Fatal(err)
 	}
 
+	battery := 1
+
 	// Read title and variables and units
 	skip := 0
 	for line := range t.Lines {
 		if strings.Contains(line.Text, "TOA5") {
 			skip = 4
 		}
-		if skip > 0 {
+		if skip == 3 {
 			// decode headers here
+			fields := readCSVLine(line.Text)
+			variates := stringSlice(fields)
+			battery = variates.pos("GageMinV")
+		}
+		if skip > 0 {
+			// skip the rest
 			skip = skip - 1
 			continue
 		}
 		// Read data
-		reader := csv.NewReader(strings.NewReader(line.Text))
-		fields, err := reader.Read()
-		if err != nil {
-			log.Fatal(err)
-		}
-		voltage, err := strconv.ParseFloat(fields[2], 64)
+		fields := readCSVLine(line.Text)
+		voltage, err := strconv.ParseFloat(fields[battery], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
