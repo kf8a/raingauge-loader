0683f3bc21f1fae137c50bd47b172eaa8014334e
Sven Bohm <bohms@msu.edu>
2016-01-07 10:28:10 -0500

    read json data from stdin to set up loggers

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"github.com/ActiveState/tail"
 	"github.com/prometheus/client_golang/prometheus"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -43,6 +45,12 @@ func (slice stringSlice) pos(value string) int {
 	return -1
 }
 
+type logger struct {
+	FileName           string `json:"file-name"`
+	Site               string `json:"site"`
+	BatteryVariateName string `json:"battery-variate-name"`
+}
+
 func readCSVLine(text string) []string {
 	reader := csv.NewReader(strings.NewReader(text))
 	fields, err := reader.Read()
@@ -52,9 +60,9 @@ func readCSVLine(text string) []string {
 	return fields
 }
 
-func loadData(fileName string, site string, batteryVoltName string) {
+func loadData(logger logger) {
 
-	t, err := tail.TailFile(fileName, tail.Config{
+	t, err := tail.TailFile(logger.FileName, tail.Config{
 		Follow: true,
 		ReOpen: true,
 	})
@@ -62,7 +70,7 @@ func loadData(fileName string, site string, batteryVoltName string) {
 		log.Fatal(err)
 	}
 
-	battery := 1
+	var batteryFieldNumber int
 
 	// Read title and variables and units
 	skip := 0
@@ -74,7 +82,7 @@ func loadData(fileName string, site string, batteryVoltName string) {
 			// decode headers
 			fields := readCSVLine(line.Text)
 			variates := stringSlice(fields)
-			battery = variates.pos(batteryVoltName)
+			batteryFieldNumber = variates.pos(logger.BatteryVariateName)
 		}
 		if skip > 0 {
 			// skip the rest
@@ -83,20 +91,28 @@ func loadData(fileName string, site string, batteryVoltName string) {
 		}
 		// Read data
 		fields := readCSVLine(line.Text)
-		voltage, err := strconv.ParseFloat(fields[battery], 64)
+		voltage, err := strconv.ParseFloat(fields[batteryFieldNumber], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
 		// log.Println(voltage)
-		batteryVoltage.WithLabelValues(site).Set(voltage)
-		loadEvents.WithLabelValues(site).Inc()
+		batteryVoltage.WithLabelValues(logger.Site).Set(voltage)
+		loadEvents.WithLabelValues(logger.Site).Inc()
 	}
 }
 
 func main() {
 
-	go loadData("raingauge_Table1.dat", "lter-raingauge", "GageMinV")
-	go loadData("MarginalLand_five_min.dat", "luxarbor", "BattV_Min")
+	dec := json.NewDecoder(os.Stdin)
+
+	var loggers []logger
+	if err := dec.Decode(&loggers); err != nil {
+		log.Println(err)
+		return
+	}
+	for _, lg := range loggers {
+		go loadData(lg)
+	}
 
 	http.Handle("/metrics", prometheus.Handler())
 	http.ListenAndServe(":9094", nil)
